965da115c427eca394df2c899ce2b26748c264b4
Sidd374 <siddhantshah3000@gmail.com>
2023-08-11 00:07:41 +0545

    .

--- inpaint.py
+++ inpaint.py
@@ -20,7 +20,6 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
 os.environ['NUMEXPR_NUM_THREADS'] = '1'
-
 import cv2
 import hydra
 import numpy as np
