7093be5a9d006bc0b58b979f23d621cb114d1e93
Sidd374 <siddhantshah3000@gmail.com>
2023-08-19 01:51:45 +0545

    .

--- backdoor.py
+++ backdoor.py
@@ -34,12 +34,71 @@ def bg_avg(dirpath):
             if i ==5:
                 break
 
-    plt.imshow(avg_image)
-    plt.show()
+    
     return avg_image
+
+def stackImagesKeypointMatching(dirpath):
+
+    orb = cv2.ORB_create()
+
+    file_list = os.listdir(dirpath)
+
+    # disable OpenCL to because of bug in ORB in OpenCV 3.1
+    cv2.ocl.setUseOpenCL(False)
+        # compute the descriptors with ORB
+    i=0
+    stacked_image = None
+    first_image = None
+    first_kp = None
+    first_des = None
+    for file in file_list:
+        image = cv2.imread(os.path.join(dirpath,file),1)
+        imageF = image.astype(np.float32) / 255
+
+        # compute the descriptors with ORB
+        kp = orb.detect(image, None)
+        kp, des = orb.compute(image, kp)
+
+        # create BFMatcher object
+        matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+        if first_image is None:
+            # Save keypoints for first image
+            stacked_image = imageF
+            first_image = image
+            first_kp = kp
+            first_des = des
+        else:
+             # Find matches and sort them in the order of their distance
+            matches = matcher.match(first_des, des)
+            matches = sorted(matches, key=lambda x: x.distance)
+
+            src_pts = np.float32(
+                [first_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32(
+                [kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+            # Estimate perspective transformation
+            try:
+                M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5.0)
+                w, h, _ = imageF.shape
+                imageF = cv2.warpPerspective(imageF, M, (h, w))
+                stacked_image += imageF
+            except:
+                pass  
+            i = i+1
+
+            if i ==5:
+                break
+
+    stacked_image /= len(file_list)
+    stacked_image = (stacked_image*255).astype(np.uint8)
+    return stacked_image
 
 for dirpath, dirnames, filenames in os.walk(inpaint_dir):
     if os.listdir(dirpath)[0].endswith('.png'):
-        out = bg_avg(dirpath)
+        out = stackImagesKeypointMatching(dirpath)
+        plt.imshow(out)
+        plt.show()
         save_path = os.path.join(bg_avg_dir, dirpath[len(inpaint_dir) +1:], dirpath.split('\\')[-1]+'.png')
         cv2.imwrite(save_path, out)
