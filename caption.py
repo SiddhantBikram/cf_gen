7a4be8f7a0e8afe541f7cf00a80c4465245eb6fc
Sidd374 <siddhantshah3000@gmail.com>
2023-08-30 00:25:08 +0545

    .

--- caption.py
+++ caption.py
@@ -1,16 +1,25 @@
 from PIL import Image
 import requests
 from transformers import AutoProcessor, BlipForConditionalGeneration
+from configs import *
+import pickle
 
 processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-text = ""
+dir = os.path.join(inpaint_dir, 'train', '01_bird')
 
-inputs = processor(images=image, text=text, return_tensors="pt")
-pixel_values = inputs.pixel_values
-generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(generated_caption)
+caps = []
+
+for img in os.listdir(dir):
+    image = Image.open(os.path.join(dir, img))
+    text = ""
+    inputs = processor(images=image, text=text, return_tensors="pt")
+    pixel_values = inputs.pixel_values
+    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(generated_caption)
+    caps.append(generated_caption)
+
+with open(os.path.join(root_dir, 'caps'), 'wb') as fp:
+    pickle.dump(caps, fp)
